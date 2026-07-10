ef15db737eb30bc8b8ade8431d321f91e65415fc
Bünyamin Epatay <epatayb@gmail.com>
2026-07-10 23:47:24 +0300

    .

---
+++ MyPortfolio/ViewComponents/_TestimonialViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _TestimonialViewComponent : ViewComponent
+
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
