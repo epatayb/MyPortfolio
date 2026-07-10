82bc7dfc203a57b6bbe628489f936154af38075b
Bünyamin Epatay <epatayb@gmail.com>
2026-07-11 02:31:19 +0300

    .

--- MyPortfolio/Program.cs
+++ MyPortfolio/Program.cs
@@ -1,4 +1,8 @@
+using MyPortfolio.DAL.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
--- MyPortfolio/ViewComponents/_FeatureViewComponent.cs
+++ MyPortfolio/ViewComponents/_FeatureViewComponent.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _FeatureViewComponent : ViewComponent
     {
+        private readonly AppDbContext _context;
+
+        public _FeatureViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var features = _context.Features.ToList();
+            return View(features);
         }
     }
 }
