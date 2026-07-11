464b4bcc9f8cc9f85ca03b36f7f7e8d46b558de9
Bünyamin Epatay <epatayb@gmail.com>
2026-07-11 13:44:35 +0300

    .

--- MyPortfolio/ViewComponents/_AboutViewComponent.cs
+++ MyPortfolio/ViewComponents/_AboutViewComponent.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _AboutViewComponent : ViewComponent
     {
+        private readonly AppDbContext _context;
+
+        public _AboutViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IViewComponentResult Invoke()
         {
+            ViewBag.aboutTitle = _context.Abouts.Select(x => x.Title).FirstOrDefault();
+            ViewBag.aboutSubDescription = _context.Abouts.Select(x => x.SubDescription).FirstOrDefault();
+            ViewBag.aboutDetail = _context.Abouts.Select(x => x.Details).FirstOrDefault();
+
             return View();
         }
     }
