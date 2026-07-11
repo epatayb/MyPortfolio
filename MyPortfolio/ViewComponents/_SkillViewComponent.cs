9c000b893f72ce5d54a304d5ea0c7255d7fc09fc
Bünyamin Epatay <epatayb@gmail.com>
2026-07-11 14:57:57 +0300

    .

--- MyPortfolio/ViewComponents/_SkillViewComponent.cs
+++ MyPortfolio/ViewComponents/_SkillViewComponent.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _SkillViewComponent : ViewComponent
     {
+        private readonly AppDbContext _context;
+
+        public _SkillViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var skills = _context.Skills.ToList();
+            return View(skills);
         }    
     }
 }
