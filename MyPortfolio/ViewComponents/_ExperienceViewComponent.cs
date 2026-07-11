c3f9a3f231661645c7a29a049c05fcf5119c13fd
Bünyamin Epatay <epatayb@gmail.com>
2026-07-11 16:38:20 +0300

    .

--- MyPortfolio/ViewComponents/_ExperienceViewComponent.cs
+++ MyPortfolio/ViewComponents/_ExperienceViewComponent.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _ExperienceViewComponent : ViewComponent
     {
+        private readonly AppDbContext _context;
+        
+        public _ExperienceViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var experiences = _context.Experiences.OrderByDescending(e => e.StartDate).ToList();
+            return View(experiences);
         }
     }
 }
