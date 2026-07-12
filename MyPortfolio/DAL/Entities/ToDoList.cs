0d3a015721da6e92d54f77503562669dd29ad1aa
Bünyamin Epatay <epatayb@gmail.com>
2026-07-13 01:52:41 +0300

    .

---
+++ MyPortfolio/DAL/Entities/ToDoList.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime Date{ get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
