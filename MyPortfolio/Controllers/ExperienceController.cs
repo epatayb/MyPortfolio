752e488c1d96ab706cdd88b99ec140e29f078932
Bünyamin Epatay <epatayb@gmail.com>
2026-07-13 01:00:32 +0300

    .

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -58,6 +58,49 @@ namespace MyPortfolio.Controllers
             return RedirectToAction("ExperienceList");
         }
 
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var experience = _context.Experiences.Find(id);
+            if (experience != null)
+            {
+                if (DateTime.TryParse(experience.StartDate, out DateTime startDate))
+                {
+                    experience.StartDate = startDate.ToString("yyyy-MM-dd");
+                }
+
+                if (!experience.IsActive && DateTime.TryParse(experience.EndDate, out DateTime endDate))
+                {
+                    experience.EndDate = endDate.ToString("yyyy-MM-dd");
+                }
+                else if (experience.IsActive)
+                {
+                    experience.EndDate = "";
+                }
+            }
+            return View(experience);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(experience);
+            }
+            if (experience.EndDate == null && !experience.IsActive)
+            {
+                ModelState.AddModelError("EndDate", "Lütfen bitiş tarihini giriniz veya 'Hala çalışıyorum' kutucuğunu işaretleyiniz.");
+                return View(experience);
+            }
+            if (experience.IsActive)
+            {
+                experience.EndDate = "Halen Devam Ediyor";
+            }
+            _context.Experiences.Update(experience);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
 
     }
 }
