d34bb1be843966ece3f6a065bfc4f3d3416d0c44
Bünyamin Epatay <epatayb@gmail.com>
2026-07-13 01:46:28 +0300

    .

---
+++ MyPortfolio/Controllers/MessageController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MessageController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Inbox()
+        {
+            var values = _context.Messages.ToList();
+            return View(values);
+        }
+        public IActionResult ChangeIsReadToTrue(int id)
+        {
+            var value = _context.Messages.Find(id);
+            value.IsRead = true;
+            _context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public IActionResult ChangeIsReadToFalse(int id)
+        {
+            var value = _context.Messages.Find(id);
+            value.IsRead = false;
+            _context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = _context.Messages.Find(id);
+            _context.Messages.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult MessageDetail(int id)
+        {
+            var value = _context.Messages.Find(id);
+            return View(value);
+        }
+    }
+}
